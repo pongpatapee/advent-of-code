67891d7e8c3b0d2d3cb7ea0fa789fd0b56bab523
pongpatapee <pongpatapee1021@gmail.com>
2023-12-12 17:46:51 -0500

    solved day10 part1

--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,4 @@
-dirs = {
+dirs_map = {
     "N": (-1, 0),
     "S": (1, 0),
     "E": (0, 1),
@@ -55,7 +55,7 @@ def infer_starting_pipe(S, matrix):
 
     can_connect_list = []
 
-    for dir_letter, dir in dirs.items():
+    for dir_letter, dir in dirs_map.items():
         r_dir, c_dir = dir
 
         if (0 <= (r + r_dir) <= len(matrix)) and (0 <= (c + c_dir) <= len(matrix[0])):
@@ -70,13 +70,47 @@ def infer_starting_pipe(S, matrix):
     return pipe_inference_map[tuple(can_connect_list)]
 
 
+def bfs(S, matrix):
+    largest_num_steps = 0
+    queue = [(S, None, 0)]
+    visited = set()
+
+    while queue:
+        curr_node, coming_from, num_step = queue.pop(0)
+        r, c = curr_node
+
+        if curr_node in visited:
+            continue
+
+        if (r < 0 or r >= len(matrix)) or (c < 0 or c >= len(matrix[0])):
+            continue
+
+        curr_pipe = matrix[r][c]
+        if coming_from and (curr_pipe not in coming_from_map[coming_from]):
+            continue
+
+        if curr_pipe == "S":
+            curr_pipe = infer_starting_pipe(curr_node, matrix)
+
+        largest_num_steps = max(largest_num_steps, num_step)
+        visited.add(curr_node)
+
+        for dir in next_valid_dir[curr_pipe]:
+            dir_r, dir_c = dirs_map[dir]
+            new_node = (r + dir_r, c + dir_c)
+            if new_node not in visited:
+                queue.append((new_node, opposite_dir_map[dir], num_step + 1))
+
+    print("largest num steps: ")
+    print(largest_num_steps)
+
+
 def solve_part1(file):
     matrix = get_inputs(file)
 
     S = find_starting_pos(matrix)
-    starting_pipe = infer_starting_pipe(S, matrix)
 
-    print(starting_pipe)
+    bfs(S, matrix)
 
 
 if __name__ == "__main__":
